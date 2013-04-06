2e546ca2b71fbce1ea38529cb757d7b39d4750c1
Robin Malburn <robin.malburn@googlemail.com>
2013-04-06 22:19:03 +0100

    Added list and show current commands

--- sublimeHamster.py
+++ sublimeHamster.py
@@ -14,6 +14,61 @@ def hamster_cli_stop():
 	"""End the current activity"""
 	subprocess.Popen(["hamster-cli", "stop"])
 
+def hamster_cli_list():
+	"""Get the list of activities from hamster-cli"""
+	process = subprocess.Popen(["hamster-cli", "list"], stdout=subprocess.PIPE)
+	communication = process.communicate()
+	response = ""
+	if communication[0] is not None:
+		response = communication[0]
+	return response
+
+def stringifiy_fact(fact):
+	"""Convert a fact object into a hamster-cli friendly string supporting activity, category, description and tags"""
+	result = ""
+
+	if fact is not None:
+		result += "{0}@{1}".format(fact["activity"], fact["category"])
+
+		if "description" in fact and len(fact["description"]) > 0:
+			result += " {0}, ".format(fact["description"])
+
+		if "tags" in fact and len(fact["tags"]) > 0:
+			result += " , "
+			for tag in fact["tags"]:
+				result += "#{0} ".format(tag)
+
+	return result
+
+class HamsterListCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		"""Show the list of Hamster activities returned by hamster-cli"""
+		hamster_list = hamster_cli_list();
+		window = sublime.active_window()
+		view = window.new_file()
+		view.set_scratch(True) # settings the view as a scratch view prevents Sublime from asking us to save on close
+		view.set_name("Hamster Activity List")
+		edit = view.begin_edit()
+		view.insert(edit, 0, hamster_list)
+		view.end_edit(edit)
+
+class HamsterCurrentCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		"""Show the current active activity"""
+		# get the hamster_dbus_bridge path from settings
+		settings = sublime.load_settings("sublime-hamster.sublime-settings")
+		bridge_path = settings.get("hamster_dbus_bridge")
+		bridge_path = bridge_path.replace("${packages}", sublime.packages_path())
+
+		process = subprocess.Popen(["python", bridge_path], stdout=subprocess.PIPE)
+		communication = process.communicate()
+		response = json.loads(communication[0])
+		active = "No Activity"
+		for fact in response:
+			if fact["endTime"] == 0:
+				active = stringifiy_fact(fact)
+
+		sublime.status_message(active)
 
 class HamsterSwitchCommand(sublime_plugin.WindowCommand):
 	def run(self):
@@ -35,32 +90,17 @@ class HamsterSwitchCommand(sublime_plugin.WindowCommand):
 			desc = fact["description"]
 
 			self.facts.append(fact)
-			messages.append(["{0}@{1}".format(activity, category), "Description: {0}, Tags: {1}".format(desc, tags)])
+			messages.append(["{0}@{1}".format(activity, category), "Description: {0} Tags: {1}".format(desc, tags)])
 
 		window = sublime.active_window()
 		window.show_quick_panel(messages, self._on_done)
 
 	def _on_done(self, index):
 		"""on_done handler, parses the chosen fact and starts the activity"""
-		fact = self.facts[index]
-		str_fact = self._stringifiy_fact(fact)
-		hamster_cli_start(str_fact)
-
-	def _stringifiy_fact(self, fact):
-		"""Convert a fact object into a hamster-cli friendly string supporting activity, category, description and tags"""
-		result = "";
-		result += "{0}@{1}".format(fact["activity"], fact["category"])
-
-		if "description" in fact:
-			result += " {0}, ".format(fact["description"])
-		elif "tags" in fact and len(fact["tags"]) > 0:
-			result += ", "
-		
-		if "tags" in fact and len(fact["tags"]) > 0:
-			for tag in fact["tags"]:
-				result += "#{0} ".format(tag)
-			
-		return result
+		if index > -1:
+			fact = self.facts[index]
+			str_fact = stringifiy_fact(fact)
+			hamster_cli_start(str_fact)
 
 class HamsterStopCommand(sublime_plugin.WindowCommand):
 	def run(self):
